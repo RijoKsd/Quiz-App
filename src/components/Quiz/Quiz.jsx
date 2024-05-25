bff5e570c5d8f5d99d754c78467c2eb263b2b26e
Rijo Sebastian <rijobdk@gmail.com>
2024-05-25 20:32:01 +0530

    complete quiz app

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,20 +1,97 @@
+import { useRef, useState } from "react";
+import { data } from "../../assets/data";
 import "./Quiz.css";
 const Quiz = () => {
+  const [index, setIndex] = useState(0);
+  const [question, setQuestion] = useState(data[index]);
+  const [lockAnswer, setLockAnswer] = useState(false);
+  const [score, setScore] = useState(0);
+  const [result, setResult] = useState(false);
+
+  const a = useRef(null);
+  const b = useRef(null);
+  const c = useRef(null);
+  const d = useRef(null);
+
+  const optionRefs = [a, b, c, d];
+
+  const checkAnswer = (element, answer) => {
+    if (lockAnswer === false) {
+      if (question.correct === answer) {
+        element.target.classList.add("correct");
+        setScore((prev) => prev + 1);
+
+        setLockAnswer(true);
+      } else {
+        element.target.classList.add("wrong");
+        setLockAnswer(true);
+      }
+
+      //   setLockAnswer(true);
+    }
+  };
+  const restartQuiz = () => {
+    setIndex(0);
+    setQuestion(data[0]);
+    setLockAnswer(false);
+    setScore(0);
+    setResult(false);
+  };
+
+  const showNextQuestion = () => {
+    if (lockAnswer) {
+      if (index < data.length - 1) {
+        setIndex((prev) => prev + 1);
+        setQuestion(data[index + 1]);
+        setLockAnswer(false);
+        optionRefs.forEach((option) => {
+          option.current.classList.remove("correct");
+          option.current.classList.remove("wrong");
+        });
+      }
+      if (index === data.length - 1) {
+        setResult(true);
+      }
+    }
+  };
+
   return (
     <div className="container">
       <h1>Quiz App</h1>
       <hr />
-      <h2>
-        1. How do you call a function named <code>myFunction</code>?
-      </h2>
-      <ul>
-        <li>myFunction() </li>
-        <li>call myFunction()</li>
-        <li>call function myFunction()</li>
-        <li>null</li>
-      </ul>
-      <button>Next</button>
-      <div className="index">1 of 5 question</div>
+      {result ? (
+        <>
+          <h1>
+            Result: {score} out of {data.length}
+          </h1>
+          <button onClick={restartQuiz}>Restart</button>
+        </>
+      ) : (
+        <>
+          {" "}
+          <h2>
+            {index + 1}. {question.question}
+          </h2>
+          <ul>
+            <li ref={a} onClick={(e) => checkAnswer(e, "a")}>
+              {question.a}
+            </li>
+            <li ref={b} onClick={(e) => checkAnswer(e, "b")}>
+              {question.b}
+            </li>
+            <li ref={c} onClick={(e) => checkAnswer(e, "c")}>
+              {question.c}
+            </li>
+            <li ref={d} onClick={(e) => checkAnswer(e, "d")}>
+              {question.d}
+            </li>
+          </ul>
+          <button onClick={showNextQuestion}>Next</button>
+          <div className="index">
+            {index + 1} of {data.length} question
+          </div>
+        </>
+      )}
     </div>
   );
 };
